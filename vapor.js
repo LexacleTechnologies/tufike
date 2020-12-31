d99ce3f99e9df7fa5b4fbf5d13761c7c4d32c236
Lexacle Technologies <lexacletechnologies@gmail.com>
2021-01-01 00:32:20 +0300

    Check if FTP issue is fixed

--- vapor.js
+++ vapor.js
@@ -40,7 +40,7 @@ const Cms = require("./models/cms.js");
 const freelance = '5f47f9e62dc7b16cb6f33c40';
 const player = require('play-sound')(opts = {})
 const Supportchat = require("./models/supportchat.js");
-const ftpConnection = { host: 'lexacle.com', port: 21, secure: false, user: 'vapor@cloud.lexcle.com', password: 'Leslie#Myles@2028' };
+const ftpConnection = { host: 'lexacle.com', port: 21, secure: false, user: 'vapor@cloud.lexacle.com', password: 'Leslie#Myles@2028' };
 const url = 'mongodb+srv://tufike:nUJjC9qzGYih8ZrX@cluster0.17g7f.mongodb.net/tufike?retryWrites=true&w=majority';
 const atsdk = { apiKey: '8d82a365b9424afffc695b8558648dc4b29b7d63b86db1313028eb4e54052209', username: 'tufike' };
 const Flutterwave = require('flutterwave-node-v3');
